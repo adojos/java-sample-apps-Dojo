5444635d28a5fe8de83e6e0ac068616474f53012
Tushar Sharma <testoxide@gmail.com>
2020-08-13 17:42:12 +0530

    email app class added

---
+++ src/main/java/com/testoxide/app/emailapp/Email.java
@@ -0,0 +1,44 @@
+package com.testoxide.app.emailapp;
+
+public class Email {
+	
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String departmentName;
+	private String mailboxCapacity;
+	private String alternateEmail;
+	
+	
+	// main method
+	
+	public static void main(String args[]) {
+		
+	}
+	
+	
+	// Constructor to receive first and last name
+	
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("Email Created : " + this.firstName + "." + this.lastName );
+		
+	}
+		
+	
+	
+	// Ask for department
+	
+	// Set mailbox capacity
+	
+	// set alternative email
+	
+	// change password
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/testoxide/app/emailapp/EmailApp.java
@@ -0,0 +1,11 @@
+package com.testoxide.app.emailapp;
+
+public class EmailApp {
+	
+	public static void main (String args[]) {
+		
+		Email email = new Email("Tushar","Sharma");
+		
+	}
+
+}
